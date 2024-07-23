85b9da854cb5416b4911563797e2618c9bb350d9
linhonghao <845003566@qq.com>
2024-07-23 11:05:13 +0800

    update

--- push.sh
+++ push.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# 获取提交信息
-echo "请输入提交信息："
-read commit_message
-
 # 添加所有更改
 git add .
 
 # 提交更改
-git commit -m "$commit_message"
+git commit -m "update"
 
 # 推送到远程仓库
 git push origin dev
